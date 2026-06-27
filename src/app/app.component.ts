759463c58e0bf8b34b071dfd812251a072a1cf1a
Julia <upogorelova626@gmail.com>
2026-06-27 04:10:12 +0300

    init Astusha ID frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {TuiRoot} from '@taiga-ui/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    imports: [RouterOutlet, TuiRoot],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'astusha-id';
+    title = 'astusha-id';
 }
