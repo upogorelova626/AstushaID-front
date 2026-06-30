2d3d9d8c9c4e301aeefd182e5b7b636864e99927
Julia <upogorelova626@gmail.com>
2026-06-30 21:34:20 +0300

    feat: add email and appearance settings

--- src/app/features/settings/components/appearance-settings-card/appearance-settings-card.component.ts
+++ src/app/features/settings/components/appearance-settings-card/appearance-settings-card.component.ts
@@ -1,8 +1,9 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {TuiButton, TuiIcon} from '@taiga-ui/core';
 
 @Component({
     selector: 'app-appearance-settings-card',
-    imports: [],
+    imports: [TuiButton, TuiIcon],
     templateUrl: './appearance-settings-card.component.html',
     styleUrl: './appearance-settings-card.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/features/settings/components/email-settings-card/email-settings-card.component.ts
+++ src/app/features/settings/components/email-settings-card/email-settings-card.component.ts
@@ -1,10 +1,16 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {TuiIcon, TuiInput, TuiLabel, TuiTextfield} from '@taiga-ui/core';
+import {AstushaUser} from '../../../../shared/interfaces';
+import {TuiSkeleton} from '@taiga-ui/kit';
 
 @Component({
     selector: 'app-email-settings-card',
-    imports: [],
+    imports: [TuiIcon, TuiInput, TuiTextfield, TuiLabel, TuiSkeleton],
     templateUrl: './email-settings-card.component.html',
     styleUrl: './email-settings-card.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EmailSettingsCardComponent {}
+export class EmailSettingsCardComponent {
+    readonly currentUser = input<AstushaUser | null>(null);
+    readonly isLoading = input(false);
+}
