82f467ca3b1b79bb6d611d932cb245c15c65a104
Julia <upogorelova626@gmail.com>
2026-07-03 01:25:45 +0300

    feat: add delete-account dialogs at security page

--- src/app/features/security-settings/components/account-delete-card/account-delete-card.component.ts
+++ src/app/features/security-settings/components/account-delete-card/account-delete-card.component.ts
@@ -1,7 +1,19 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {TuiButton} from '@taiga-ui/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    inject,
+    Injector
+} from '@angular/core';
+import {
+    TuiButton,
+    TuiDialogService,
+    TuiNotificationService
+} from '@taiga-ui/core';
 import {TuiAvatar} from '@taiga-ui/kit';
 import {TuiList} from '@taiga-ui/layout';
+import {PolymorpheusComponent} from '@taiga-ui/polymorpheus';
+import {DeleteProfileNotificationDialogComponent} from '../../../profile/components/profile-quick-actions/delete-profile-notification-dialog/delete-profile-notification-dialog.component';
+import {switchMap} from 'rxjs';
 
 @Component({
     selector: 'app-account-delete-card',
@@ -10,4 +22,22 @@ import {TuiList} from '@taiga-ui/layout';
     styleUrl: './account-delete-card.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AccountDeleteCardComponent {}
+export class AccountDeleteCardComponent {
+    private readonly alerts = inject(TuiNotificationService);
+    private readonly dialogs = inject(TuiDialogService);
+    private readonly injector = inject(Injector);
+
+    protected openDeleteProfileNotificationDialog() {
+        this.dialogs
+            .open<boolean>(
+                new PolymorpheusComponent(
+                    DeleteProfileNotificationDialogComponent,
+                    this.injector
+                ),
+                {
+                    size: 's'
+                }
+            )
+            .subscribe();
+    }
+}
