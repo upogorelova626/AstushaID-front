5539d4bf37148c5aeafb180cd14a501b66a1afac
Julia <upogorelova626@gmail.com>
2026-07-05 01:15:48 +0300

    feat: add logout at sidebar

--- src/app/shared/components/layout-sidebar/layout-sidebar.component.ts
+++ src/app/shared/components/layout-sidebar/layout-sidebar.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {TuiButton, TuiIcon} from '@taiga-ui/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {AuthService} from '../../../features/auth/services/auth.service';
 
 @Component({
     selector: 'app-layout-sidebar',
@@ -9,4 +10,13 @@ import {RouterLink} from '@angular/router';
     styleUrl: './layout-sidebar.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LayoutSidebarComponent {}
+export class LayoutSidebarComponent {
+    private readonly autnService = inject(AuthService);
+    private readonly router = inject(Router);
+
+    protected logout() {
+        this.autnService
+            .logout()
+            .subscribe(() => this.router.navigate(['/auth/login']));
+    }
+}
