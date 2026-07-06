029b2d8cd6bf042f7761902fcdacf4e472a425f2
Julia <upogorelova626@gmail.com>
2026-07-06 23:40:06 +0300

    feat: add support contact dialog

---
+++ src/app/shared/components/help-daialog/help-daialog.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {TuiButton, TuiIcon, type TuiDialogContext} from '@taiga-ui/core';
+import {TuiAvatar} from '@taiga-ui/kit';
+import {injectContext} from '@taiga-ui/polymorpheus';
+
+@Component({
+    selector: 'app-help-daialog',
+    imports: [TuiIcon, TuiButton, TuiAvatar],
+    templateUrl: './help-daialog.component.html',
+    styleUrl: './help-daialog.component.less'
+})
+export class HelpDaialogComponent {
+    protected readonly context = injectContext<TuiDialogContext<void, void>>();
+}
--- src/app/shared/components/layout-sidebar/layout-sidebar.component.ts
+++ src/app/shared/components/layout-sidebar/layout-sidebar.component.ts
@@ -1,7 +1,9 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {TuiButton, TuiIcon} from '@taiga-ui/core';
+import {TuiButton, TuiDialogService, TuiIcon} from '@taiga-ui/core';
 import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../../features/auth/services/auth.service';
+import {PolymorpheusComponent} from '@taiga-ui/polymorpheus';
+import {HelpDaialogComponent} from '../help-daialog/help-daialog.component';
 
 @Component({
     selector: 'app-layout-sidebar',
@@ -13,10 +15,20 @@ import {AuthService} from '../../../features/auth/services/auth.service';
 export class LayoutSidebarComponent {
     private readonly autnService = inject(AuthService);
     private readonly router = inject(Router);
+    private readonly dialogs = inject(TuiDialogService);
 
     protected logout() {
         this.autnService
             .logout()
             .subscribe(() => this.router.navigate(['/auth/login']));
+    }
+
+    protected openHelpDialog() {
+        this.dialogs
+            .open<void>(new PolymorpheusComponent(HelpDaialogComponent), {
+                size: 's'
+            })
+
+            .subscribe();
     }
 }
